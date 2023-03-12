5fb2a0c993b96ae5c80055bb46ed451aae41439c
M-Shams <mahdishams.13811381@gmail.com>
2023-03-12 12:21:53 +0330

    creat k lower

--- quick select/Program.cs
+++ quick select/Program.cs
@@ -9,57 +9,48 @@ namespace quick_select
         static void Main(string[] args)
         {
             int[] r = { 10, 4, 5, 8, 6, 11, 26 };
-           Console.WriteLine(k_LowerElemnt(r,3));
         }
-        // sowap tow element together
-        #region sowap
-        public static void sowap(int[] array, int arraynum1, int arraynum2)
+        public static void sowap(int[] array, int index1, int index2)
         {
-            int temp = array[arraynum1];
-            array[arraynum1] = array[arraynum2];
-            array[arraynum2] = temp;
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
         }
-        #endregion
-        //find the pivot (pivot is number that som numbers are biger than its and som numbers are lower than its)
-        #region pivot
-        public static int FindPivot(int[] array)
+        public static int pivot(int[] array)
         {
-            int pivot = 0;// that goes from 0 to last if the element be lower than its 
-            for (int index = 0; index < array.Length - 1; index++)
+            int pivot = array[0];
+            for (int index = 0; index < array.Length; index++)
             {
-                // if this element be lower than last element
-                if (array[0] < array[array.Length - 1])
+                if (array[index] < array[array.Length - 1])
                 {
                     sowap(array, index, pivot);
                     pivot++;
                 }
             }
             sowap(array, pivot, array.Length - 1);
-            //return pivot element 
             return pivot;
         }
-        #endregion
-        public static int k_LowerElemnt(int[] array, int k)
+        public static int k_LowerElement(int[]array , int k)
         {
-            if (k > array.Length || array.Length == 0 || k < 0)
+            if (k > array.Length || k < 0)
             {
                 throw new Exception();
             }
-            int pivot = FindPivot(array);
-            if (k == pivot)
+           int piv =  pivot(array);
+            if(piv == k)
             {
-                return pivot;
+
             }
-            else if (k < pivot)
+            else if(piv < k)
             {
-                int[] array2 = array.Select(n => n).Skip(array.Length-pivot+1).ToArray();
-                return k_LowerElemnt(array2, k);
+                int[] a = array.Select(n => n).SkipLast(piv).ToArray();
             }
             else
             {
-                int[] array2 = array.Select(n => n).SkipLast(pivot-1).ToArray();
-                return k_LowerElemnt(array2, k);
+
+                int[] a = array.Select(n => n).Skip(array.Length - piv).ToArray();KD
             }
         }
     }
+
 }
