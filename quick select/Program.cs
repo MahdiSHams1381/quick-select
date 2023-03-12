1c7acc71696d2d0cafae1ce5b4bede9c49e01f56
M-Shams <mahdishams.13811381@gmail.com>
2023-03-12 12:16:11 +0330

    fix pivot function and add sowao function

--- quick select/Program.cs
+++ quick select/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace quick_select
@@ -7,44 +8,58 @@ namespace quick_select
     {
         static void Main(string[] args)
         {
-            int[] r = { 10, 41, 5, 8, 6, 11, 26 };
-            int a = FindPivot(r);
-            Console.WriteLine(a);
+            int[] r = { 10, 4, 5, 8, 6, 11, 26 };
+           Console.WriteLine(k_LowerElemnt(r,3));
         }
-        // pivot is number that  is biger than some numbers in array and lower than other
-        public static int FindPivot(int[] Array)
+        // sowap tow element together
+        #region sowap
+        public static void sowap(int[] array, int arraynum1, int arraynum2)
         {
-            // if array dont have any element
-            if (Array.Length == 0)
+            int temp = array[arraynum1];
+            array[arraynum1] = array[arraynum2];
+            array[arraynum2] = temp;
+        }
+        #endregion
+        //find the pivot (pivot is number that som numbers are biger than its and som numbers are lower than its)
+        #region pivot
+        public static int FindPivot(int[] array)
+        {
+            int pivot = 0;// that goes from 0 to last if the element be lower than its 
+            for (int index = 0; index < array.Length - 1; index++)
             {
-               throw new Exception("array is not valiabel");
-            }
-            // if array have one element
-            if (Array.Length == 1)
-            {
-               return Array[0];
-            }
-            int PivotLocal = 0; // when element is lower than end element
-            int temp;
-            for (int index = 0 ; index < Array.Length-1 ; index++)
-            {
-                // sowap the element index and PivotLocal
-                if(Array[index] < Array[Array.Length - 1])
+                // if this element be lower than last element
+                if (array[0] < array[array.Length - 1])
                 {
-                    temp = new int() ;
-                    temp = Array[index];
-                    Array[index] = Array[PivotLocal];
-                    Array[PivotLocal] = temp;
-                    PivotLocal++;
+                    sowap(array, index, pivot);
+                    pivot++;
                 }
             }
-             temp = new int();
-            temp = Array[Array.Length - 1];
-            Array[Array.Length-1] = Array[PivotLocal];
-            Array[Array.Length - 1] = temp;
-            return Array[PivotLocal];
-
+            sowap(array, pivot, array.Length - 1);
+            //return pivot element 
+            return pivot;
         }
-
+        #endregion
+        public static int k_LowerElemnt(int[] array, int k)
+        {
+            if (k > array.Length || array.Length == 0 || k < 0)
+            {
+                throw new Exception();
+            }
+            int pivot = FindPivot(array);
+            if (k == pivot)
+            {
+                return pivot;
+            }
+            else if (k < pivot)
+            {
+                int[] array2 = array.Select(n => n).Skip(array.Length-pivot+1).ToArray();
+                return k_LowerElemnt(array2, k);
+            }
+            else
+            {
+                int[] array2 = array.Select(n => n).SkipLast(pivot-1).ToArray();
+                return k_LowerElemnt(array2, k);
+            }
+        }
     }
 }
